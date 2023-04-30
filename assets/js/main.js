f09a6d0acbd5afe38f2c36f5426d95bc843027a8
jan1ooo <filhojanio67@gmail.com>
2023-04-30 20:12:23 -0300

    Adicionando novos pokemons, listando os pokemons pelo JS

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,8 +1,34 @@
 const offset = 0;
-const limit = 6;
-const url = `https://pokeapi.co/api/v2/pokemon?offset=${offset}&limit=${limit}`;
+const limit = 15;
+const url = `https://pokeapi.co/api/v2/pokemon?offset=${offset}&limit=${limit}`
+
+function convertPokemonToLi(pokemon) {
+    return `
+        <li class="pokemon">
+            <span class="number">#001</span>
+            <span class="name">${pokemon.name}</span>
+            <div class="detail">
+                <ol class="types">
+                    <li class="type">grass</li>
+                    <li class="type">poison</li>
+                </ol>
+                <img src="assets/img/${pokemon.name}.png" alt="${pokemon.name}">
+            </div>
+        </li>
+    `
+}
+
+const pokemonList = document.getElementById('pokemonList')
+
 
 fetch(url)
     .then((response) => response.json())
-    .then((jsonBody) => console.log(jsonBody))
+    .then((jsonBody) => jsonBody.results)
+    .then((pokemons) => {
+        for (let i = 0; i < pokemons.length; i++) {
+            const pokemon = pokemons[i];
+            pokemonList.innerHTML += convertPokemonToLi(pokemon)
+            
+        }
+    })
     .catch((error) => console.error(error))
